cc41fa83c5d6a987fe3d34d4dedb7e8e57f7eccd
Mohit Keshri <sendtomohit99@gmail.com>
2024-08-28 10:59:28 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -2,28 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
-const cron = require("node-cron");
+const jobs = require("./utils/job");
 
 //const { sendBasicEmail } = require("./services/email-service");
+const TicketController = require("./controller/ticket-controller");
 
 const setupAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.post("/api/v1/tickets", TicketController.create);
+
   app.listen(PORT, () => {
     console.log(`Server Started on port ${PORT}`);
-
+    jobs();
     // sendBasicEmail(
     //   "support@admin.com",
     //   "mohitkeshri069@gmail.com",
     //   "This is a testing email",
     //   "Hey ,how are you ?"
     // );
-
-    cron.schedule("*/2 * * * *", () => {
-      console.log("running a task every two minutes");
-    });
   });
 };
 
